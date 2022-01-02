4192e06510eb5aba5b3c67bb9cb414dc870c6a33
thennek0 <kennethbecaro0@gmail.com>
2022-01-02 20:11:08 +0800

    added game functionality

---
+++ script.js
@@ -0,0 +1,48 @@
+
+function computerPlay(){
+    let choice = Math.floor((Math.random() * 3) + 1);
+    switch (choice) {
+        case 1:
+            return "rock";
+            break;
+        case 2:
+            return "paper";
+            break;
+        case 3:
+            return "scissors";
+            break;    
+    }
+}
+
+function singleRound(playerSelection = "",computerChoice){
+    let a = playerSelection.toLowerCase();
+    let b = computerChoice();
+    console.log(a,b);
+
+
+
+
+    if (a === b) {
+        console.log("Its a tie!");
+    }
+    else{
+        if (a === "rock" && b === "scissors") {
+            console.log("win!");
+        }
+        else if (a === "paper" && b === "rock") {
+            console.log("win!");
+            
+        }
+        else if (a === "scissors" && b === "paper") {
+            console.log("win!");
+        }
+        else{
+            console.log("lose!");
+            return "lose!";
+        }
+    }
+
+}
+let choice = prompt("enter your choice");
+
+singleRound(choice,computerPlay);
